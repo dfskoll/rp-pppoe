89bd5f1f89e2dec987d07178ab7392ac7bbe320f
Dianne Skoll <dianne@skoll.ca>
2023-01-18 18:51:00 -0500

    Make md5 code use stdint.h uint32_t type.

--- src/md5.h
+++ src/md5.h
@@ -5,18 +5,10 @@
  */
 
 #include "config.h"
-
-#if SIZEOF_UNSIGNED_INT == 4
-typedef unsigned int uint32;
-#elif SIZEOF_UNSIGNED_LONG == 4
-typedef unsigned long uint32;
-#else
-# error Could not find a 32-bit integer type
-#endif
-
+#include <stdint.h>
 struct MD5Context {
-	uint32 buf[4];
-	uint32 bits[2];
+	uint32_t buf[4];
+	uint32_t bits[2];
 	unsigned char in[64];
 };
 
@@ -24,7 +16,7 @@ void MD5Init(struct MD5Context *context);
 void MD5Update(struct MD5Context *context, unsigned char const *buf,
 	       unsigned len);
 void MD5Final(unsigned char digest[16], struct MD5Context *context);
-void MD5Transform(uint32 buf[4], uint32 const in[16]);
+void MD5Transform(uint32_t buf[4], uint32_t const in[16]);
 
 /*
  * This is needed to make RSAREF happy on some MS-DOS compilers.
